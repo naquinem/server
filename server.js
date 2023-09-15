44201f016b92ffc589b39f9ed6c2b9ca7af9b2be
naquinem <marlonagba@gmail.com>
2023-09-15 11:53:08 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+import express, { json } from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+//create a app variable to use the cors and cookieparser in express
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+
+//Make a variable querry for database connection
+// I used mysql database with port 3307 and i already create my employeedb database in my PHP my Admin XAMPP server
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: '3307',
+    database: 'employeedb',
+});
+
+//I used this code to test my database connection
+db.connect(function(err) {
+
+    if (err) {
+        console.log("Error Connection");
+    } else {
+        console.log("Connected");
+    }
+});
+
+//Post the employee data in MySQL database
+app.get('/user', (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, result) => {
+        if (err) return res.json({Message: "Error in server"});
+        return res.json(result);
+    })
+});
+
+app.listen(8000, ()=> {
+    console.log("Listening to port 8000");
+});
+
